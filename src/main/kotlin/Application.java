3cd0292829262c5f4ec88d434ff46891d77b934d
Swetha Murali <swetham@thoughtworks.com>
2022-11-23 22:53:45 +0530

    check master to main conversion

--- src/main/kotlin/Application.java
+++ src/main/kotlin/Application.java
@@ -1,5 +1,6 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("test conversion from master to main");
     }
 }
